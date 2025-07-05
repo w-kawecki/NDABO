5752c67f93b804040b25432d0e2723797be7cd39
Władysław Kawecki <w-kawecki@example.com>
2025-07-05 12:42:57 +0200

    fixed node.js server issues

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const path = require('path');
-const app = express();
 
+const app = express();
 const port = process.env.PORT || 3000;
 
-// Serwowanie wszystkich plików z katalogu głównego
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Aplikacja działa na porcie ${port}`);
+  console.log(`Serwer działa na porcie ${port}`);
 });
